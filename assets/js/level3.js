c669e1fb4bbdf86bf708dd353844b0b748fc70a3
Hanmb17 <>
2023-07-29 19:08:30 +0000

    Add level 4 js file and loadLevel function

---
+++ assets/js/level3.js
@@ -0,0 +1,15 @@
+// level3.js
+// Export function to add level 3 detail to the game
+export function loadLevel() {
+    // Set the starting lights for level 3
+    gridSize = 4;
+    setUpGrid();
+    startingLights = [2, 3, 4, 8, 10, 12];
+    lightsToBeTurnedOn(startingLights);
+
+    // Add the "water" class to all the lights
+    var allLights = document.querySelectorAll('.light');
+    allLights.forEach(function(light) {
+        light.classList.add('earth');
+    });
+}
---
+++ assets/js/level4.js
@@ -0,0 +1,16 @@
+// level4.js
+// Export function to add level 4 detail to the game
+export function loadLevel() {
+    // Set grid size for level 4
+    gridSize = 5;
+    setUpGrid();
+    // Set the starting lights for level 4
+    startingLights = [2, 5, 6, 10, 18, 20, 21, 22, 23];
+    lightsToBeTurnedOn(startingLights);
+
+    // Add the "water" class to all the lights
+    var allLights = document.querySelectorAll('.light');
+    allLights.forEach(function(light) {
+        light.classList.add('air');
+    });
+}
