58e31dfe20852f32aa0aef0fe793073e49af08f5
Hanmb17 <>
2023-07-29 09:50:21 +0000

    Add LoadLevel function

---
+++ assets/js/level2.js
@@ -0,0 +1,15 @@
+// level2.js
+// Export function to add level 1 detail to the game
+export function loadLevel() {
+    // Set the starting lights for level 2
+    gridSize = 4;
+    setUpGrid();
+    startingLights = [1, 2, 3, 8, 9, 10, 11, 12];
+    lightsToBeTurnedOn(startingLights);
+
+    // Add the "water" class to all the lights
+    var allLights = document.querySelectorAll('.light');
+    allLights.forEach(function(light) {
+        light.classList.add('water');
+    });
+}
