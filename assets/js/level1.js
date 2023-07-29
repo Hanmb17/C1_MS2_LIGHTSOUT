73e738df71157fb9310c936a8d4e3a0b082aeb89
Hanmb17 <>
2023-07-29 09:56:07 +0000

    Add setUpgrid function and  Add setUpgrid function and information to loadLevel function for level 1

--- assets/js/level1.js
+++ assets/js/level1.js
@@ -2,6 +2,9 @@
 // Export function to add level 1 detail to the game
 export function loadLevel() {
     // Set the starting lights for level 1
+    gridSize === 3;
+    setUpGrid();
+    
     startingLights = [1, 3, 4, 5, 6];
     lightsToBeTurnedOn(startingLights);
 
